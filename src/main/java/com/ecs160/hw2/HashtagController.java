790dd4827c8a5936163118a2919ccefc7c516a14
Scott Wu <SoHowsLife924@gmail.com>
2025-03-10 23:03:51 -0700

    Added Hashtag Microservice

---
+++ src/main/java/com/ecs160/hw2/HashtagController.java
@@ -0,0 +1,45 @@
+package com.ecs160.hw2;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.github.ollama4j.OllamaAPI;
+import io.github.ollama4j.models.response.OllamaResult;
+import io.github.ollama4j.types.OllamaModelType;
+import io.github.ollama4j.utils.OptionsBuilder;
+
+import java.util.List;
+
+@RestController
+public class HashtagController {
+    private final String promptTemp = "Please generate a hashtag for this social media post. Only reply with the hashtags:";
+    @PostMapping("/hashtag")
+    public String hashtag(@RequestBody MyRequest request) {
+        String host = "http://localhost:11434/";
+        OllamaAPI ollamaAPI = new OllamaAPI(host);
+        ollamaAPI.setRequestTimeoutSeconds(30);
+        String prompt = promptTemp + request.getPostContent();
+        try {
+            String modelName = OllamaModelType.LLAMA3;
+            OllamaResult response = ollamaAPI.generate(modelName, prompt, false, new OptionsBuilder().build());
+
+            return response.getResponse();
+
+        } catch (Exception e) {
+            return "#bskypost";
+        }
+    }
+
+    static class MyRequest {
+        private String postContent;
+
+        public String getPostContent() {
+            return postContent;
+        }
+
+        public void setPostContent(String postContent) {
+            this.postContent = postContent;
+        }
+    }
+}
--- src/moderation/java/com/ecs160/hw2/ModerationController.java
+++ src/moderation/java/com/ecs160/hw2/ModerationController.java
@@ -4,6 +4,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.List;
 
 @RestController
@@ -21,7 +25,19 @@ public class ModerationController {
                 return "FAILED";
             }
         }
-        return "SUCCESS";
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest hashtag = HttpRequest.newBuilder()
+                    .uri(new URI("http://localhost:30002/hashtag"))
+                    .header("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString("{\"postContent\": \"" + request.getPostContent() + "\"}"))
+                    .build();
+
+            HttpResponse<String> response = client.send(hashtag, HttpResponse.BodyHandlers.ofString());
+            return response.body();
+        } catch (Exception e) {
+            return "#bskypost";
+        }
     }
 
     static class MyRequest {
